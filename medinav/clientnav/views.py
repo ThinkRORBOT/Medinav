61fe59248296f54abcf61bfc5a143998089355b0
ThinkRORBot <person645h@gmail.com>
2021-05-28 21:43:01 +1000

    login page working

--- medinav/clientnav/views.py
+++ medinav/clientnav/views.py
@@ -1,17 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-# from .models import SurveyResponse, TouchGesture, MotionSensor, SurveyCode, TextSurveyResponse, SwipeSurveyResponse
+from .models import usrCredentials, mixedDrugs, ingrediantList, drugIngrediant 
 from django.views.decorators.csrf import csrf_exempt
 import logging
 
 
 logger = logging.getLogger(__name__)
 # Create your views here.
-
+@csrf_exempt
 def index(request):
+    if request.method == 'POST':
+        response_dict = request.POST.dict()
+        password = response_dict['pwd']
+        username = response_dict['u_name']
+        
+        usr_credentials = usrCredentials();
+        num = usrCredentials.objects.filter(u_name=username, u_pwd=password)
+        print(num.count())
+        if (num.count() == 1):
+            return JsonResponse({'status':'200'})
+        elif (num.count() == 0):
+            return JsonResponse({'status':'401'})
     return render(request, 'clientnav/index.html')
 
+@csrf_exempt
 def get_index(request):
+
     return render(request, 'clientnav/index.html')
 
 def get_help(request):
